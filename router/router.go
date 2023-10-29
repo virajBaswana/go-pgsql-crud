fe9042b3919ea843ec08b936c837d44eaeb3305d
virajBaswana <virajbaswana@gmail.com>
2023-10-29 22:52:21 +0530

    correct WSL system time to reflect accurate time stamps in git commits

--- router/router.go
+++ router/router.go
@@ -9,11 +9,11 @@ import (
 func Router() *mux.Router {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/stocks/{id}", middleware.GetStock).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/stocks", middleware.GetAllStocks).Methods("GET", "OPTIONS")
+	// router.HandleFunc("/api/stocks/{id}", middleware.GetStock).Methods("GET", "OPTIONS")
+	// router.HandleFunc("/api/stocks", middleware.GetAllStocks).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/newStock", middleware.CreateStock).Methods("POST", "OPTIONS")
-	router.HandleFunc("/api/stocks/{id}", middleware.UpdateStock).Methods("PUT", "OPTIONS")
-	router.HandleFunc("/api/stocks/{id}", middleware.DeleteStock).Methods("DELETE", "OPTIONS")
+	// router.HandleFunc("/api/stocks/{id}", middleware.UpdateStock).Methods("PUT", "OPTIONS")
+	// router.HandleFunc("/api/stocks/{id}", middleware.DeleteStock).Methods("DELETE", "OPTIONS")
 
 	return router
 }
